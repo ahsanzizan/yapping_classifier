275839fb5de7e09880cbce15d351f984d90af168
Ahsan <ahsanaz461@gmail.com>
2024-05-14 20:07:57 +0700

    Fine-tune model a bit, use cleaned text for training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import pickle
-
 import tensorflow as tf
+import utils
 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
@@ -15,39 +15,7 @@ tokenizer = None
 with open('./models/tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-# Hyperparameters
-VOCAB_SIZE = 1000
-EMBEDDING_DIM = 16
-MAX_LENGTH = 20
-TRUNCATING_TYPE = 'post'
-PADDING_TYPE = 'post'
-OOV_TOKEN = "<OOV>"
-
-
-def preprocess_texts(texts: list[str]):
-    # Use the saved tokenizer to tokenize the given text
-    sequences = tokenizer.texts_to_sequences(texts)
-    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-        sequences, maxlen=MAX_LENGTH, padding=PADDING_TYPE, truncating=TRUNCATING_TYPE)
-    return padded_sequences
-
-
-def classify_text(model, text: str):
-    # Pre-processed texts must be a list for the model to consume
-    preprocessed_text = preprocess_texts([text])
-    # Use the saved model to make a prediction on the Pre-processed text
-    prediction = model.predict(preprocessed_text)
-
-    prediction = prediction.flatten()
-    negative_confidence = 1 - prediction[0]
-    print(
-        f"Negative Confidence: {negative_confidence * 100:.3f}%\nPositive Confidence: {(1 - negative_confidence) * 100:.3f}%")
-
-    # Classify the text as either Negative or Positive
-    return "positive" if prediction >= .5 else "negative"
-
-
 if __name__ == '__main__':
     input_text = input("Your yapp > ")
     print(
-        f"The yapp is classified as a {classify_text(model, input_text)} text")
+        f"The yapp is classified as a {utils.classify_text(model, tokenizer, input_text)} yapp")
---
+++ utils.py
@@ -0,0 +1,64 @@
+import re
+
+import tensorflow as tf
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+# Hyperparameters
+VOCAB_SIZE = 1000
+EMBEDDING_DIM = 16
+MAX_LENGTH = 20
+TRUNCATING_TYPE = 'post'
+PADDING_TYPE = 'post'
+OOV_TOKEN = "<OOV>"
+
+# Preprocessing
+stop_words = stopwords.words("english")
+stemmer = SnowballStemmer("english")
+
+
+def clean_text(text: str):
+    # Casefolding and remove extra spaces
+    output = text.lower().strip()
+
+    # Remove extra spaces in between
+    output = re.sub(' +', ' ', output)
+
+    # Remove @username
+    output = re.sub('@\w+', ' ', output)
+
+    # Remove punctuations
+    output = re.sub('[^a-zA-Z]', ' ', output)
+
+    # Remove all words with only one char in it
+    output = re.sub('\b\w\b', '', output)
+
+    # Remove stopwords and stemming
+    output = ' '.join(stemmer.stem(word)
+                      for word in output.split() if word not in stop_words)
+
+    return output
+
+
+def preprocess_texts(tokenizer, texts: list[str]):
+    # Use the saved tokenizer to tokenize the given text
+    cleaned_texts = [clean_text(text) for text in texts]
+    sequences = tokenizer.texts_to_sequences(cleaned_texts)
+    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
+        sequences, maxlen=MAX_LENGTH, padding=PADDING_TYPE, truncating=TRUNCATING_TYPE)
+    return padded_sequences
+
+
+def classify_text(model, tokenizer, text: str):
+    # Pre-processed texts must be a list for the model to consume
+    preprocessed_text = preprocess_texts(tokenizer, [text])
+    # Use the saved model to make a prediction on the Pre-processed text
+    prediction = model.predict(preprocessed_text)
+
+    prediction = prediction.flatten()
+    negative_confidence = 1 - prediction[0]
+    print(
+        f"Negative Confidence: {negative_confidence * 100:.3f}%\nPositive Confidence: {(1 - negative_confidence) * 100:.3f}%")
+
+    # Classify the text as either Negative or Positive
+    return "positive" if prediction >= .5 else "negative"
